98f5b3fe5656f47240a463a44d4112b19f5ded67
Marcus Suda <114450653+marcussuda@users.noreply.github.com>
2025-02-20 22:35:41 +0000

    Criei o primeiro desafio novato Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,60 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    char estado; //variável para colocar nome do estado
+    char codigo [3]; //variável para colocar o código da carta
+    char nome [30]; //variável para colocar o nome da cidade
+    int populacao; //variável para colocar a quantidade da população
+    float area; //variável para colocar a área que a cidade ocupa
+    float pib; //variável para colocar o PIB da cidade
+    int pontos; //variável para colocar quantos pontos turísticos existem na cidade
+
+    printf("CARTA 01 \n");//carta 01 do jogo
+
+    printf("Estado: "); //saída de dados representa qual o estado na carta
+    scanf("%s", &estado); //entrada de dados para se colocar o estado
+
+    printf("Código da Carta: "); //saída de dados representa qual o codigo da carta
+    scanf("%s", &codigo); //entrada de dados para se colocar o código da carta com uma letra e dois números
+
+    printf("Nome da Cidade: "); //saída de dados representa qual o Nome da Cidade da carta
+    scanf("%s", &nome); //entrada de dados para se colocar o Nome da cidade na carta
+
+    printf("População: "); //saída de dados representa qual a População da cidade
+    scanf("%d", &populacao); //entrada de dados para se colocar quantidade da população 
+
+    printf("Área km²: "); //saída de dados representa qual a área em km² da cidade
+    scanf("%d", &area); //entrada de dados para se colocar a área que a cidade ocupa
+
+    printf("PIB Per Capita: "); //saída de dados representa qual o PIB per capita da cidade
+    scanf("%d", &pib); //entrada de dados para se colocar o PIB per capita
+
+    printf("Número de Pontos Turísticos: "); //saída de dados representa qual a quantidade de pontos turísticos da cidade
+    scanf("%d", &pontos) ; //entrada de dados para se colocar qual a quantidade de pontos turísticos da cidade
+
+    printf("\nCARTA 02 \n");//carta 02 do jogo
+
+    printf("Estado: "); //saída de dados representa qual o estado na carta
+    scanf("%s", &estado); //entrada de dados para se colocar o estado
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Código da Carta: "); //saída de dados representa qual o codigo da carta
+    scanf("%s", &codigo); //entrada de dados para se colocar o código da carta com uma letra e dois números
+    
+    printf("Nome da Cidade: "); //saída de dados representa qual o Nome da Cidade da carta
+    scanf("%s", &nome); //entrada de dados para se colocar o Nome da cidade na carta
+    
+    printf("População: "); //saída de dados representa qual a População da cidade
+    scanf("%d", &populacao); //entrada de dados para se colocar quantidade da população 
+    
+    printf("Área km²: "); //saída de dados representa qual a área em km² da cidade
+    scanf("%d", &area); //entrada de dados para se colocar a área que a cidade ocupa
+    
+    printf("PIB Per Capita: "); //saída de dados representa qual o PIB per capita da cidade
+    scanf("%d", &pib); //entrada de dados para se colocar o PIB per capita
+    
+    printf("Número de Pontos Turísticos: "); //saída de dados representa qual a quantidade de pontos turísticos da cidade
+    scanf("%d", &pontos); //entrada de dados para se colocar qual a quantidade de pontos turísticos da cidade
 
     return 0;
 }
